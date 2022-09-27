d5c5dd80d55a777989c732c2655da832f8728a04
Yu <yulliepcruz@gmail.com>
2022-09-27 12:48:45 -0300

    style: crew and tech pages

--- src/pages/Technology/index.jsx
+++ src/pages/Technology/index.jsx
@@ -1,7 +1,36 @@
 import "./styles.css";
+import data from "../../data.json";
+import { useState } from "react";
 
-export default function Technology () {
-    return (
-        <h1>Tech</h1>
-    )
+export default function Technology() {
+  const tech = data.technology;
+  const [technology] = useState(tech);
+  const [value, setValue] = useState(0);
+  const { name, images, description } = technology[value];
+
+  return (
+    <div className="tech__container">
+      <div className="title-container">
+        <h1 className="tech__title">Space Launch 101</h1>
+      </div>
+      <div className="content-container">
+        <div className="tech__buttons">
+          {technology.map((item, index) => (
+            <button key={index} onClick={() => setValue(index)}>
+              {index + 1}
+            </button>
+          ))}
+        </div>
+
+        <div className="tech__info">
+          <h2 className="tech__subtitle">The Terminology...</h2>
+          <h2 className="tech__name">{name}</h2>
+          <h2 className="tech__description">{description}</h2>
+        </div>
+        <div className="tech__picture">
+          <img src={images.portrait} alt={name} />
+        </div>
+      </div>
+    </div>
+  );
 }
